faba8709c1850855047455f8fa7d79c8524364fa
Renato Vieira <renato.pacheco.r2@gmail.com>
2020-03-02 17:13:26 -0300

    estilizacao cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,66 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Produto</th>
+            <th>Qtd</th>
+            <th>Subtotal</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://imgcentauro-a.akamaihd.net/900x900/M02ZFU02/tenis-feminino-preto-torani-com-glitter-img.jpg"
+                alt="tenis"
+              />
+            </td>
+            <td>
+              <strong>Tenis maneirao</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button>
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button>
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button>
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button>Finalizar Pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
